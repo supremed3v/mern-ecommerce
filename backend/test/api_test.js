e76bc02192ee6454fa8097deba30e4612d96002e
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-12-29 00:36:51 +0500

    added forgot password route and controller (tested)

--- backend/test/api_test.js
+++ backend/test/api_test.js
@@ -3,30 +3,32 @@ const chaiHttp = require("chai-http");
 const expect = chai.expect;
 const baseUrl = "http://localhost:3333";
 
+const email = "test2@mail.com";
+
 chai.use(chaiHttp);
-describe("API TEST", () => {
-  it("POST /api/v1/register", (done) => {
-    chai
-      .request(baseUrl)
-      .post("/api/v1/register")
-      .send({
-        name: "test",
-        email: "test2@mail.com",
-        password: "test",
-        address: "test address",
-        mobile: "1234567890",
-      })
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.a("object");
-        expect(res.body).to.have.property("success");
-        expect(res.body).to.have.property("newUser");
-        expect(res.body).to.have.property("token");
-        done();
-        if (err) return done(err.message);
-      });
-  });
-}, 10000);
+// describe("API TEST", () => {
+//   it("POST /api/v1/register", (done) => {
+//     chai
+//       .request(baseUrl)
+//       .post("/api/v1/register")
+//       .send({
+//         name: "test",
+//         email: "test2@mail.com",
+//         password: "test",
+//         address: "test address",
+//         mobile: "1234567890",
+//       })
+//       .end((err, res) => {
+//         expect(res).to.have.status(200);
+//         expect(res.body).to.be.a("object");
+//         expect(res.body).to.have.property("success");
+//         expect(res.body).to.have.property("newUser");
+//         expect(res.body).to.have.property("token");
+//         done();
+//         if (err) return done(err.message);
+//       });
+//   });
+// }, 10000);
 
 describe("API TEST", () => {
   it("POST /api/v1/login", (done) => {
@@ -47,4 +49,35 @@ describe("API TEST", () => {
         if (err) return done(err.message);
       });
   });
-}, 10000);
+  it("POST /api/v1/logout", (done) => {
+    chai
+      .request(baseUrl)
+      .post("/api/v1/logout")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.have.property("success");
+        expect(res.body).to.have.property("message").equal("Come back soon!");
+        done();
+        if (err) return done(err.message);
+      });
+  });
+  it("POST /api/v1/password/forgot", (done) => {
+    chai
+      .request(baseUrl)
+      .post("/api/v1/password/forgot")
+      .send({
+        email: email,
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.have.property("success");
+        expect(res.body)
+          .to.have.property("message")
+          .equal(`Email sent to: ${email}`);
+        done();
+        if (err) return done(err.message);
+      });
+  });
+}, 500000);
