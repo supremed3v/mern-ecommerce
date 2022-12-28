429570f58ede26c3e0c398adfe185baf6da21ca2
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-12-28 13:24:42 +0500

    added test library and testing for API

---
+++ backend/test/api_test.js
@@ -0,0 +1,29 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
+const baseUrl = "http://localhost:3333";
+
+chai.use(chaiHttp);
+describe("API TEST", () => {
+  it("POST /api/v1/register", (done) => {
+    chai
+      .request(baseUrl)
+      .post("/api/v1/register")
+      .send({
+        name: "test",
+        email: "testing@mail.com",
+        password: "test",
+        address: "test address",
+        mobile: "1234567890",
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.have.property("success");
+        expect(res.body).to.have.property("newUser");
+        expect(res.body).to.have.property("token");
+        done();
+        if (err) return done(err.message);
+      });
+  });
+});
