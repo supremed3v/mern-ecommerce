9f72cec193658540aa2efff77d5ed1e2d8288f47
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-12-28 23:55:20 +0500

    added route for logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -71,3 +71,14 @@ export const loginUser = async (req, res, next) => {
     });
   }
 };
+
+export const logoutUser = async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Come back soon!",
+  });
+};
