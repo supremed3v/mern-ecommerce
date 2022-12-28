64e5ae11a2c7956945914a9a693a67c2b990c500
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-12-28 23:49:33 +0500

    added route and unit test(route working)

--- backend/test/api_test.js
+++ backend/test/api_test.js
@@ -11,7 +11,7 @@ describe("API TEST", () => {
       .post("/api/v1/register")
       .send({
         name: "test",
-        email: "testing@mail.com",
+        email: "test2@mail.com",
         password: "test",
         address: "test address",
         mobile: "1234567890",
@@ -26,4 +26,25 @@ describe("API TEST", () => {
         if (err) return done(err.message);
       });
   });
-});
+}, 10000);
+
+describe("API TEST", () => {
+  it("POST /api/v1/login", (done) => {
+    chai
+      .request(baseUrl)
+      .post("/api/v1/login")
+      .send({
+        email: "testing@mail.com",
+        password: "test",
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.have.property("success");
+        expect(res.body).to.have.property("newUser");
+        expect(res.body).to.have.property("token");
+        done();
+        if (err) return done(err.message);
+      });
+  });
+}, 10000);
