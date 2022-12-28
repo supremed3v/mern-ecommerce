5e8c01a2886bf5404c5f4b4ef61d0e9342b86eac
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-12-29 00:43:35 +0500

    Added sendEmail.js to middlewares folder

---
+++ backend/middlewares/sendEmail.js
@@ -0,0 +1,27 @@
+import nodeMailer from "nodemailer";
+
+const sendEmail = async (options) => {
+  // 1) Create a transporter
+  const transporter = nodeMailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: "Admin <  >",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions);
+};
+
+export default sendEmail;
