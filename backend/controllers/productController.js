9f880cb237168cad14a5a53f2a6e9a82284a7465
supremed3v <saadsiddiqui-programmer@protonmail.com>
2022-12-30 03:54:08 +0500

    added api features for pagination, filtering and searching. Added productController for crud functionality

---
+++ backend/controllers/productController.js
@@ -0,0 +1,201 @@
+import Product from "../models/Product.Model.js";
+import ApiFeatures from "../middlewares/apiFeature.js";
+
+export const createProduct = async (req, res, next) => {
+  // TODO - add image upload functionality
+
+  req.body.user = req.user.id;
+
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+export const getAllProducts = async (req, res, next) => {
+  const resultPerPage = 8;
+  const productCount = await Product.countDocuments();
+
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+
+  let products = await apiFeatures.query;
+  let filteredProductsCount = products.length;
+  apiFeatures.pagination(resultPerPage);
+  products = await apiFeatures.query;
+
+  res.status(200).json({
+    success: true,
+    productCount,
+    products,
+    resultPerPage,
+    filteredProductsCount,
+  });
+};
+
+export const getAdminProducts = async (req, res, next) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+export const getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+export const updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      nessage: "Can't update product that doesn't exist",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+export const deletProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Can't delete product that doesn't exist",
+    });
+  }
+
+  await product.remove();
+  res.status(200).json({
+    success: true,
+    message: "Successfully deleted product",
+  });
+};
+
+export const createProductReview = async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  // Check if the user has already reviewed the product
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (isReviewed) {
+    product.reviews.forEach((rev) => {
+      if (rev.user.toString() === req.user._id.toString()) {
+        rev.comment = comment;
+        rev.rating = rating;
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+  }
+
+  let avg = 0;
+  product.reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+  product.ratings = avg / product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+};
+
+export const getProductReviews = async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+};
+
+export const deleteProductReview = async (req, res, next) => {
+  const product = await Product.findById(req.query.productId);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  const reviews = product.reviews.filter(
+    (rev) => rev._id.toString() !== req.query.id.toString()
+  );
+
+  let avg = 0;
+
+  reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+  let ratings = 0;
+  if (reviews.length !== 0) {
+    ratings = 0;
+  } else {
+    ratings = avg / reviews.length;
+  }
+
+  const numOfReviews = reviews.length;
+
+  await Product.findByIdAndUpdate(
+    req.query.productId,
+    {
+      reviews,
+      ratings,
+      numOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+  });
+};
---
+++ backend/middlewares/apiFeature.js
@@ -0,0 +1,40 @@
+class ApiFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  search() {
+    const keyword = this.queryString.keyword
+      ? {
+          name: {
+            $regex: this.queryString.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    this.query = this.query.find({ ...keyword });
+  }
+
+  filter() {
+    const queryCopy = { ...this.queryString };
+    // Removing fields from the query
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => delete queryCopy[el]);
+    // Advance filter for price, ratings etc
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  pagination(resultPerPage) {
+    const currentPage = Number(this.queryString.page) || 1;
+    const skip = resultPerPage * (currentPage - 1);
+    this.query = this.query.limit(resultPerPage).skip(skip);
+    return this;
+  }
+}
+
+export default ApiFeatures;
